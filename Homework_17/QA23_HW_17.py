11d6f6df59a8a53a9110aad5f846966c497ea2e5
prokopenkoga1977 <prokopenkoga1977@gmail.com>
2023-02-18 00:53:00 +0200

    Added code according homework_17

---
+++ Homework_17/QA23_HW_17.py
@@ -0,0 +1,25 @@
+# 1.
+# 1) Get user name
+# 2) Greet user by name
+
+# 2.
+# 1) Get user email
+# 2) Print user email into the console
+
+
+# 3.
+# 1.) Get user year of birth
+# 2.) Print year of birth to the console
+
+#User name
+user_name=input("Enter your user name: ")
+print("Hello "+user_name+"!")
+
+#User email
+user_email=input("Enter your email: ")
+print("Email: "+user_email)
+
+#Year of birth
+user_yearbirth=input("Enter your year of birth: ")
+print("Year of birth: "+user_yearbirth)
+
